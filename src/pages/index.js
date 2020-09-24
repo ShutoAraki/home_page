daa7f3d3934a2afd868b183b5a184104bbc481f5
ShutoAraki <shutoaraki@gmail.com>
2020-09-24 15:55:31 +0900

    Original text change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,36 +5,33 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
-import ReactTextTransition, { presets } from 'react-text-transition';
+// import ReactTextTransition, { presets } from 'react-text-transition';
 
 const features = [
   {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: <>Passionate</>,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        I am passionate about mathematics and technology! I believe mathematics can help us understand 
+        the complex structures of the universe(s) and technology can turn our creative ideas into reality.
       </>
     ),
   },
   {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: <>Curious</>,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        I am a very curious and self-driven individual. In my free time, I love reading books to travel through different
+        minds.
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: <>Biased for Action</>,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        I strongly believe in the idea of "bias for action." We need to take action and learn as we move rather than
+        sit and think too much before doing anything.
       </>
     ),
   },
@@ -59,7 +56,9 @@ function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   const [textIndex, setIndex] = useState(0);
-  const texts = ["Data Scientist",
+  const texts = ["Mathematician",
+                 "Computer Scientist",
+                 "Data Scientist",
                  "Machine Learning Engineer",
                  "DevOps Engineer",
                  "MLOps Engineer",
@@ -69,24 +68,24 @@ function Home() {
   useEffect(() => {
     setInterval(() => {
       setIndex(textIndex + 1)
-    }, 1000);
+    }, 4000);
   });
 
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={siteConfig.title}
       description="Description will go into a meta tag in <head />">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">Solutions Architect at Amazon Web Services specializing in Machine Learning and Quantum Computing</p>
-          <p className="hero__subtitle">I am a 
-            <ReactTextTransition
+          <p className="hero__subtitle">I am a {texts[textIndex % texts.length]}
+            {/* <ReactTextTransition
               text={texts[textIndex % texts.length]}
               spring={presets.gentle}
               style={{ margin: "0 6px" }}
               inline
-            />
+            /> */}
           </p>
           <div className={styles.buttons}>
             <Link
